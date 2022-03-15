3c52299bd512e155c441761dd7a62a855785f4d9
Lewis E <luisaeniebla@gmail.com>
2022-03-15 16:50:11 -0700

    got rid of client repo and added it back

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {Container} from '@material-ui/core';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
+
+
+const App = () => {
+    const user = JSON.parse(localStorage.getItem('profile'));
+    console.log(user);
+    
+    return(
+        <BrowserRouter>
+            <Container maxWidth="xl">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={ <Navigate replace to="/posts" /> } />
+                    <Route path="/posts" exact element={<Home />} />
+                    <Route path="/posts/search" exact element={<Home />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/auth" element={(!user ? <Auth /> : <Navigate to="/posts" />) } />
+                </Routes>
+            </Container>
+        </BrowserRouter>
+        
+    );
+}
+
+export default App;
